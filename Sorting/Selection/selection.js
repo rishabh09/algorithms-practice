bb35812e8fc07f18a5a727867cbb17cb19e6cbea
Rishabh Mishra <mishrarishabh865@gmail.com>
2018-12-04 12:16:03 +0530

    add comments to selection sort

--- Sorting/Selection/selection.js
+++ Sorting/Selection/selection.js
@@ -1,11 +1,16 @@
 const selectionSort = input => {
+  // make new array, so old array will not get mutated
   const arr = [...input];
   const arrLength = arr.length;
+  // iterate through every element of array
   for (let i = 0; i < arrLength; i++) {
     let minimum = i;
+    // set minimum to i
     for (let j = i + 1; j < arrLength; j++) {
+      // update minimum if element is less
       if (arr[j] < arr[minimum]) minimum = j;
     }
+    // swap element with minimum index if value is not same
     if (i !== minimum) [arr[i], arr[minimum]] = [arr[minimum], arr[i]];
   }
   return arr;
