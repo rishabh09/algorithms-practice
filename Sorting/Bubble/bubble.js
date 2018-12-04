900133c1b3b3d892365c3ceb577deca8f9b1b7a7
Rishabh Mishra <mishrarishabh865@gmail.com>
2018-12-04 11:59:51 +0530

    add comments in bubble sort

--- Sorting/Bubble/bubble.js
+++ Sorting/Bubble/bubble.js
@@ -1,7 +1,10 @@
 const bubbleSort = input => {
+  // make new array, so old array will not get mutated
   const arr = [...input];
   const arrLength = arr.length;
+  // iterate through every element of array
   for (let i = 0; i < arrLength; i++) {
+    // decrease the length, because the last element will be in correct postion after each iteration
     const stop = arrLength - i;
     for (let j = 0; j < stop; j++) {
       if (arr[j] > arr[j + 1]) [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
