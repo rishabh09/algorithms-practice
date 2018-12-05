0abed4eeb6c57c9ec8680dee775ec841ba41520b
Rishabh Mishra <mishrarishabh865@gmail.com>
2018-12-05 10:10:57 +0530

    add binary search to find last and first occurance

--- Searching/BinarySearch/binary-search.js
+++ Searching/BinarySearch/binary-search.js
@@ -15,4 +15,45 @@ const binarySearch = (arr, value) => {
   return arr[middle] === value ? middle : -1;
 };
 
-module.exports = binarySearch;
+const binarySearchLast = (arr, value) => {
+  let start = 0;
+  let end = arr.length - 1;
+
+  let middle = Math.floor((start + end) / 2);
+
+  let result = middle;
+  while (start <= end) {
+    if (arr[middle] === value) {
+      result = middle;
+      start = middle + 1;
+    } else if (arr[middle] < value) {
+      start = middle + 1;
+    } else {
+      end = middle - 1;
+    }
+    middle = Math.floor((start + end) / 2);
+  }
+  return arr[result] === value ? result : -1;
+};
+
+const binarySearchFirst = (arr, value) => {
+  let start = 0;
+  let end = arr.length - 1;
+
+  let middle = Math.floor((start + end) / 2);
+  let result = middle;
+  while (start <= end) {
+    if (arr[middle] === value) {
+      result = middle;
+      end = middle - 1;
+    } else if (arr[middle] < value) {
+      start = middle + 1;
+    } else {
+      end = middle - 1;
+    }
+    middle = Math.floor((start + end) / 2);
+  }
+  return arr[result] === value ? result : -1;
+};
+
+module.exports = { binarySearch, binarySearchLast, binarySearchFirst };
--- Searching/BinarySearch/binary-search.test.js
+++ Searching/BinarySearch/binary-search.test.js
@@ -1,4 +1,4 @@
-const binarySearch = require("./binary-search");
+const { binarySearch, binarySearchLast, binarySearchFirst } = require("./binary-search");
 
 describe("Binary Search", () => {
   it("should return index of element", () => {
@@ -7,5 +7,17 @@ describe("Binary Search", () => {
 
   it("should return -1 is element is not present", () => {
     expect(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9], 20)).toBe(-1);
+  });
+});
+
+describe("Binary Search Last", () => {
+  it("should return last occurence element", () => {
+    expect(binarySearchLast([1, 2, 3, 4, 7, 7, 7, 8, 9], 7)).toBe(6);
+  });
+});
+
+describe("Binary Search First", () => {
+  it("should return fast occurence element", () => {
+    expect(binarySearchFirst([1, 2, 7, 7, 7, 7, 7, 8, 9], 7)).toBe(2);
   });
 });
