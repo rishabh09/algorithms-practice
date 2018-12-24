d4ba83171b4a43352bc6db0487455e8926ec5d26
Rishabh Mishra <mishrarishabh865@gmail.com>
2018-12-24 10:03:27 +0530

    Add comments in rotated binary search

--- Searching/BinarySearchRotated/binary-search.js
+++ Searching/BinarySearchRotated/binary-search.js
@@ -9,20 +9,30 @@ const binarySearch = (arr, value) => {
       break;
     }
     if (arr[start] < arr[middle]) {
+// check if first part of array is sorted
       if (arr[start] <= value && value < arr[middle]) {
+// item is in the first part
+// change end to end of first part
         end = middle - 1;
       } else {
+// item is not in the first part, change start to start of second part
         start = middle + 1;
       }
     } else {
+// second part of array is sorted
       if (arr[middle] <= value && value < arr[end]) {
+// item is in the second part of array
+// change start to start of second part
         start = middle + 1;
       } else {
+// item is not in second part, change end to end of first part
         end = middle - 1;
       }
     }
+// recalculate middle with new start and end
     middle = Math.floor((start + end) / 2);
   }
+// check if item is found, otherwise return -1
   return arr[middle] === value ? middle : -1;
 };
 
